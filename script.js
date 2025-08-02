ed87026fd4319ead814d4550e851ef2f6b26e6bb
hdjunior0927 <hdjunior1210@gmail.com>
2025-08-02 01:15:53 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("requestForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  const subdomain = e.target.subdomain.value;
-  const email = e.target.email.value;
-  document.getElementById("mensaje").innerText = 
-    `Gracias por tu solicitud: ${subdomain}.domainlatinplus.com. Te contactaremos a ${email}`;
-  e.target.reset();
-});
